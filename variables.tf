8843aee6cd5bb660f4a7be98d7b577e578b6c9c8
wahhaj007 <wahhaj.malik@gmail.com>
2023-07-14 18:38:40 -0700

    Added variables.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "username" {
+  type        = string
+  description = "Aviatrix Controller's Username"
+  default     = ""
+ }
+
+ variable password {
+  description = "Aviatrix Controller's Password"
+  default = ""
+ }
+
+ variable "controller_ip" {
+  description = "Aviatrix Controller's IP Address"
+  default = ""
+ }
+
+variable "aws_account_name" {
+  type        = string
+  description = "AWS Account Name"
+  default = ""
+}
+
+variable "aws_region" {
+  description = "Pick AWS Region. Default is Oregon"
+  default     = "us-west-2"
+}
